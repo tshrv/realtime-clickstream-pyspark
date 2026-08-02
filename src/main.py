8131686956a7bec9232df118ea43b86bcf510e20
Tushar Srivastava <tusharsrivastava162@gmail.com>
2026-08-02 20:04:06 +0000

    Use spark session through context manager

--- src/main.py
+++ src/main.py
@@ -1,19 +1,25 @@
+from contextlib import contextmanager
+
 from loguru import logger
 from pyspark.sql import SparkSession
 
 
+@contextmanager
 def get_spark_session():
     spark = SparkSession.builder.appName('ClickstreamAnalytics').config(
         "spark.jars.packages",
         "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.9",
     ).getOrCreate()
     logger.info(f"Running spark version: {spark.version}")
-    return spark
+    yield spark
+    spark.stop()
 
 
 def main():
     logger.info("Hello from realtime-clickstream-pyspark!")
-    spark = get_spark_session()
+    with get_spark_session() as spark:
+        logger.info("Spark session created successfully.")
+        # Add your Spark processing logic here
 
 
 
